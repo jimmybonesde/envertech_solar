68718d19c62948e83c746c257af69d86da2f95e7
JImmyBOnes <webmaster@jimmybones.de>
2025-08-12 11:01:19 +0200

    Update config_flow.py

--- custom_components/envertech_solar/config_flow.py
+++ custom_components/envertech_solar/config_flow.py
@@ -1,7 +1,8 @@
 from homeassistant import config_entries
 import voluptuous as vol
-
 from .const import DOMAIN
+
+DEFAULT_UPDATE_INTERVAL = 60
 
 class EnvertechConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -14,5 +15,26 @@ class EnvertechConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("station_id"): str
-            })
+            }),
         )
+
+class EnvertechOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required(
+                    "update_interval",
+                    default=self.config_entry.options.get("update_interval", DEFAULT_UPDATE_INTERVAL),
+                ): vol.All(vol.Coerce(int), vol.Range(min=30, max=3600)),
+            }),
+        )
+
+def async_get_options_flow(config_entry):
+    return EnvertechOptionsFlowHandler(config_entry)
