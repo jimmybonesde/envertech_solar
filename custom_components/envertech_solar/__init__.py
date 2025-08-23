8dc3f26fecaf00e19131dfc2d4fabde55678d8b1
JImmyBOnes <webmaster@jimmybones.de>
2025-08-23 18:22:55 +0200

    Update __init__.py

--- custom_components/envertech_solar/__init__.py
+++ custom_components/envertech_solar/__init__.py
@@ -1,6 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.config_entry_flow import config_entry_only_config_schema
 from .const import DOMAIN
+
+# Damit Hassfest weiß, dass YAML nicht unterstützt wird
+CONFIG_SCHEMA = config_entry_only_config_schema
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the integration via configuration.yaml (not used here)."""
