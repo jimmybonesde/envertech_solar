d46b6eaff088b936da0435168654edb18faaff81
JImmyBOnes <webmaster@jimmybones.de>
2026-01-31 11:36:06 +0100

    Add config schema and update options flow handler

--- custom_components/envertech_solar/__init__.py
+++ custom_components/envertech_solar/__init__.py
@@ -1,18 +1,26 @@
+"""The Envertech integration."""
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN
 from .sensor import EnvertechDataUpdateCoordinator
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Integration via configuration.yaml (optional)."""
     return True
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up integration from config entry."""
     update_interval = entry.options.get("update_interval", 30)
     station_id = entry.data.get("station_id")
+
     coordinator = EnvertechDataUpdateCoordinator(hass, station_id, update_interval)
 
     try:
@@ -25,11 +33,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload config entry."""
     return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
 
-def async_get_options_flow(config_entry):
+
+def async_get_options_flow(config_entry: ConfigEntry):
     """OptionsFlow Handler für update_interval."""
     from .config_flow import EnvertechOptionsFlowHandler
     return EnvertechOptionsFlowHandler(config_entry)
