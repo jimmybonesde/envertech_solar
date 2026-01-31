f0636bf558c7835dd54ee5e61fe433c8e097fcfd
JImmyBOnes <webmaster@jimmybones.de>
2026-01-31 11:26:56 +0100

    Refactor config flow and options handling

--- custom_components/envertech_solar/config_flow.py
+++ custom_components/envertech_solar/config_flow.py
@@ -1,32 +1,42 @@
 from homeassistant import config_entries
 import voluptuous as vol
+
 from .const import DOMAIN
 
-DEFAULT_UPDATE_INTERVAL = 30  # seconds
+DEFAULT_UPDATE_INTERVAL = 30
+
 
 class EnvertechConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
             return self.async_create_entry(
                 title="Envertech Solar",
                 data=user_input,
-                options={"update_interval": DEFAULT_UPDATE_INTERVAL}
+                options={"update_interval": DEFAULT_UPDATE_INTERVAL},
             )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("station_id"): str
-            }),
+            data_schema=vol.Schema(
+                {
+                    vol.Required("station_id"): str,
+                }
+            ),
+            description_placeholders={
+                "example_url": "https://www.envertecportal.com/terminal/systemhistory/YOUR_ID?sn=...",
+                "id_placeholder": "YOUR_ID",
+            },
         )
 
 
 class EnvertechOptionsFlowHandler(config_entries.OptionsFlow):
-    """OptionsFlow für update_interval."""
+    """Options flow handler for update_interval."""
 
-    def __init__(self, config_entry):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
@@ -35,16 +45,20 @@ class EnvertechOptionsFlowHandler(config_entries.OptionsFlow):
 
         return self.async_show_form(
             step_id="init",
-            data_schema=vol.Schema({
-                vol.Required(
-                    "update_interval",
-                    default=self.config_entry.options.get(
-                        "update_interval", DEFAULT_UPDATE_INTERVAL
-                    )
-                ): vol.All(vol.Coerce(int), vol.Range(min=10, max=3600))
-            }),
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "update_interval",
+                        default=self.config_entry.options.get(
+                            "update_interval", DEFAULT_UPDATE_INTERVAL
+                        ),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=10, max=3600)),
+                }
+            ),
         )
 
 
-def async_get_options_flow(config_entry):
+def async_get_options_flow(
+    config_entry: config_entries.ConfigEntry,
+) -> EnvertechOptionsFlowHandler:
     return EnvertechOptionsFlowHandler(config_entry)
