71191d0ef63e16990614dc97d7df634123dcab53
JImmyBOnes <webmaster@jimmybones.de>
2026-09-05 11:57:23 +0200

    Clean up coordinator data on unload

--- custom_components/envertech_solar/__init__.py
+++ custom_components/envertech_solar/__init__.py
@@ -35,8 +35,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    return unload_ok
 
 
 def async_get_options_flow(config_entry: ConfigEntry):
